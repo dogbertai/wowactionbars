911c6d0981c4fd041505cd1533c336b6c6206955
dogbertai <dogbertai@users.noreply.github.com>
2020-10-27 01:36:13 -0400

    add bindings to copy script

--- copymacros.py
+++ copymacros.py
@@ -25,7 +25,8 @@ def copy_to_wtf():
     src = os.path.join(masterfolder, 'macros-cache.txt')
     dst = os.path.join(basefolder)
     shutil.copy(src, dst)
-    src = os.path.join(masterfolder, 'bindings-cache.txt')
+    src = os.path.join(masterfolder, 'bindings-cache.wtf')
+    shutil.copy(src, dst)
     for clas,chars in classdict.items():
         src = os.path.join(masterfolder, clas, 'macros-cache.txt')
         for char in chars:
@@ -37,13 +38,16 @@ def copy_to_master():
     src = os.path.join(basefolder, 'macros-cache.txt')
     dst = os.path.join(masterfolder)
     shutil.copy(src, dst)
-    src = os.path.join(basefolder, 'bindings-cache.txt')
+    src = os.path.join(basefolder, 'bindings-cache.wtf')
+    shutil.copy(src, dst)
     for clas,chars in classdict.items():
         dst = os.path.join(masterfolder, clas)
         os.makedirs(dst, exist_ok=True)
         for char in chars:
             src = os.path.join(basefolder, char, 'macros-cache.txt')
             shutil.copy(src, dst)
+            # only first char is treated as master
+            break
 
 
 if __name__ == "__main__":
